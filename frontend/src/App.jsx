5c274ee03a642000355b9243b47eba2e9cb587b3
Osamaabdul <ibrahimabdulosama@gmail.com>
2025-05-27 01:46:25 +0300

    Added a Home page and added the route in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,16 @@ import Quiz from '../pages/Quiz.jsx'
 import PrivateRoute from './components/PrivateRoute.jsx'
 import Register from '../pages/Register.jsx';
 import TakeQuiz from '../pages/TakeQuiz.jsx'
-
+import Home from '../pages/Home.jsx'
 
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/take-quiz" element={<PrivateRoute><TakeQuiz /></PrivateRoute>} />
       <Route path="/upload-pdf" element={<PrivateRoute><Quiz /></PrivateRoute>} />
     </Routes>
