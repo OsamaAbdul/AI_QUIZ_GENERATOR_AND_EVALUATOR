eb994b13124e6b990e8d9509f6a20a77109319d9
Osamaabdul <ibrahimabdulosama@gmail.com>
2025-05-27 13:54:26 +0300

    added toast alert to the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,40 @@
-import './App.css'
-import Login from '../pages/Login.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Quiz from '../pages/Quiz.jsx'
-import PrivateRoute from './components/PrivateRoute.jsx'
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login.jsx';
 import Register from '../pages/Register.jsx';
-import TakeQuiz from '../pages/TakeQuiz.jsx'
-import LandingPage from '../pages/LandingPage.jsx'
-
+import Quiz from '../pages/Quiz.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import TakeQuiz from '../pages/TakeQuiz.jsx';
+import LandingPage from '../pages/LandingPage.jsx';
+import { UserProvider } from '../src/UseContext.jsx'; // Import UserProvider
 
 function App() {
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/take-quiz" element={<PrivateRoute><TakeQuiz /></PrivateRoute>} />
-      <Route path="/upload-pdf" element={<PrivateRoute><Quiz /></PrivateRoute>} />
-    </Routes>
-   </BrowserRouter>
-  )
+    <UserProvider>
+      <BrowserRouter>
+        <ToastContainer
+          position="top-center"
+          autoClose={3000}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="light"
+          toastStyle={{ backgroundColor: '#fff', color: '#333', border: '1px solid #ccc', borderRadius: '6px' }}
+          progressStyle={{ background: '#4a90e2' }}
+        />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/take-quiz" element={<PrivateRoute><TakeQuiz /></PrivateRoute>} />
+          <Route path="/upload-pdf" element={<PrivateRoute><Quiz /></PrivateRoute>} />
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  );
 }
 
-export default App
+export default App;
